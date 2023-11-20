b15539d6a54c1d9a389542c1f4507fe3a4118801
gutobottega <guto9bottega@gmail.com>
2023-11-20 04:47:52 -0300

    done

--- main.py
+++ main.py
@@ -1,234 +1,254 @@
-# algoritmo simulação de fila simples
-# usa input:
-# - o intervalo de tempo de chegada dos clientes na fila;
-# - o intervalo de tempo de saida de um cliente na fila;
-# - número de servidores;
-# - capacidade da fila.
+import json
+import random
 
-# print("Digite o intervalo de tempo de chegada dos clientes na fila: ")
-# tempoChegadaA = int(input('De:'))
-# tempoChegadaB = int(input('Até:'))
-# print("Intervalo de tempo de chegada dos clientes na fila: ",
-#       tempoChegadaA, "até", tempoChegadaB)
-# print("Digite o intervalo de tempo de saida de um cliente na fila: ")
-# tempoSaidaA = int(input('De:'))
-# tempoSaidaB = int(input('Até:'))
-# print("Intervalo de tempo de saida de um cliente na fila: ",
-#       tempoSaidaA, "até", tempoSaidaB)
-# numeroServidores = int(input("Digite o número de servidores: "))
-# capacidadeFila = int(input("Digite a capacidade da fila: "))
-
-tempoInicial = 2.5
-
-# Fila 1
-tempoChegadaA1 = 1
-tempoChegadaB1 = 3
-tempoSaidaA1 = 2
-tempoSaidaB1 = 4
-numeroServidores1 = 2
-capacidadeFila1 = 3
-# Fila 2
-tempoSaidaA2 = 1
-tempoSaidaB2 = 3
-numeroServidores2 = 1
-capacidadeFila2 = 5
-
-# Números randomicos que serão usados na simulação - 100.000
+# Lista para armazenar números aleatórios
 numerosRandomicos = []
 
-# Simulação
-simulacao = {
-    'evento': '-',
-    'fila1': 0,
-    'fila2': 0,
-    'tempo': 0,
-    'estadoFila1': [0] * (capacidadeFila1 + 1),
-    'estadoFila2': [0] * (capacidadeFila2 + 1),
-}
+# Definição da classe Fila, que representa uma fila no sistema
 
-# Escalonador
-escalonador = [
-    {'evento': 'ch1', 'tempo': tempoInicial}
-]
 
-# Perda de clientes
-perdaFila1 = 0
-perdaFila2 = 0
+class Fila:
+    def __init__(self, id, nome, tempoAtendimento, tempoChegada, numeroServidores, capacidadeFila, probabilidadeRoteamento):
+        # Atributos da fila
+        self.id = id
+        self.nome = nome
+        self.tempoAtendimento = tempoAtendimento
+        self.tempoChegada = tempoChegada
+        self.numeroServidores = numeroServidores
+        self.probabilidadeRoteamento = probabilidadeRoteamento
+        if capacidadeFila == -1:
+            self.capacidadeFila = 10
+            self.estadoFila = [0] * 11
+        else:
+            self.capacidadeFila = capacidadeFila
+            self.estadoFila = [0] * (capacidadeFila + 1)
+        self.tamanhoAtual = 0
+        self.perda = 0
+
+    # Método para simular a chegada de um elemento à fila
+    def chegadaFila(self, tempo):
+        global simulacao
+        global filas
+
+        diferencaTempo = tempo - simulacao['tempo']
+        simulacao['tempo'] = tempo
+
+        for fila in filas:
+            fila.estadoFila[fila.tamanhoAtual] += diferencaTempo
+
+        # Escolhe aleatoriamente a rota do elemento na fila
+        rota = random.choices(list(self.probabilidadeRoteamento.keys()),
+                              weights=list(self.probabilidadeRoteamento.values()))[0]
+
+        # Se a rota for "saida", adiciona o evento de saída ao escalonador
+        if rota == "saida":
+            addEscalonador({
+                'evento': 'saida',
+                'fila': self.id,
+            })
+        else:
+            # Caso contrário, encaminha o elemento para a fila de destino
+            fila_destino = next(fila for fila in filas if fila.nome == rota)
+            # Se ainda houver capacidade na fila, adiciona um novo elemento à fila
+            if (self.capacidadeFila > self.tamanhoAtual or self.capacidadeFila == -1):
+                self.tamanhoAtual += 1
+                # Se ainda houver servidores disponíveis, adiciona o evento de passagemFila ao escalonador
+                if (self.tamanhoAtual <= self.numeroServidores):
+                    addEscalonador({
+                        'evento': 'passagemFila',
+                        'fila': self.id,
+                        'destino': fila_destino.id,
+                    })
+            else:
+                self.perda += 1
+        if len(numerosRandomicos) == 0:
+            return
+        addEscalonador({
+            'evento': 'chegada',
+            'fila': self.id,
+        })
+
+        # Método para simular a saída de um elemento da fila
+    def saidaFila(self, tempo):
+        global simulacao
+        global filas
+
+        diferencaTempo = tempo - simulacao['tempo']
+        simulacao['tempo'] = tempo
+
+        for fila in filas:
+            fila.estadoFila[fila.tamanhoAtual] += diferencaTempo
+
+        self.tamanhoAtual -= 1
+
+    # Método para simular o passasgem de um elemento na fila
+
+    def passagemFila(self, tempo, destino):
+        global simulacao
+        global filas
+
+        diferencaTempo = tempo - simulacao['tempo']
+        simulacao['tempo'] = tempo
+
+        for fila in filas:
+            fila.estadoFila[fila.tamanhoAtual] += diferencaTempo
+
+        # Reduz o tamanho da fila após o processamento
+        self.tamanhoAtual -= 1
+        if (self.tamanhoAtual >= self.numeroServidores):
+            rota = random.choices(list(self.probabilidadeRoteamento.keys()),
+                                  weights=list(self.probabilidadeRoteamento.values()))[0]
+            if rota == "saida":
+                addEscalonador({
+                    'evento': 'saida',
+                    'fila': self.id,
+                })
+            else:
+                # Caso contrário, encaminha o elemento para a fila de destino
+                fila_destino = next(
+                    fila for fila in filas if fila.nome == rota)
+                addEscalonador({
+                    'evento': 'passagemFila',
+                    'fila': self.id,
+                    'destino': fila_destino.id,
+                })
+        # Se ainda houver capacidade na fila, adiciona um novo elemento à fila
+        if (filas[destino].tamanhoAtual < filas[destino].capacidadeFila or self.capacidadeFila == -1):
+            filas[destino].tamanhoAtual += 1
+            # Se ainda houver servidores disponíveis, adiciona o evento de passagemFila ao escalonador
+            if filas[destino].tamanhoAtual <= filas[destino].numeroServidores:
+                # Escolhe aleatoriamente a rota do novo elemento na fila
+                rota = random.choices(list(filas[destino].probabilidadeRoteamento.keys()),
+                                      weights=list(filas[destino].probabilidadeRoteamento.values()))[0]
+
+                if rota == "saida":
+                    addEscalonador({
+                        'evento': 'saida',
+                        'fila': filas[destino].id,
+                    })
+                else:
+                    # Caso contrário, encaminha o elemento para a fila de destino
+                    fila_destino = next(
+                        fila for fila in filas if fila.nome == rota)
+                    addEscalonador({
+                        'evento': 'passagemFila',
+                        'fila': filas[destino].id,
+                        'destino': fila_destino.id,
+                    })
+        else:
+            filas[destino].perda += 1
+# Função para carregar parâmetros de configuração do arquivo JSON
+
+
+def carregaParametros():
+    with open('config.json', 'r') as file:
+        parametros = json.load(file)
+    return parametros
+
+
+def sorteio(A, B):
+    A, B = float(A), float(B)
+    if numerosRandomicos:
+        return A + (B - A) * numerosRandomicos.pop(0)
+
+# Função para adicionar eventos ao escalonador
+
+
+def addEscalonador(evento):
+    global escalonador
+    if evento['evento'] == 'chegada':
+        # Gera um tempo de chegada aleatório e adiciona ao escalonador
+        tempoChegada = sorteio(
+            filas[evento['fila']].tempoChegada.split('/')[0],
+            filas[evento['fila']].tempoChegada.split('/')[1]) + simulacao['tempo']
+        escalonador.append(
+            {'evento': 'chegada', 'tempo': tempoChegada, 'fila': evento['fila']})
+    elif evento['evento'] == 'passagemFila':
+        # Gera um tempo de processamento aleatório e adiciona ao escalonador
+        tempoSaida = sorteio(
+            filas[evento['fila']].tempoAtendimento.split('/')[0],
+            filas[evento['fila']].tempoAtendimento.split('/')[1]) + simulacao['tempo']
+        escalonador.append({'evento': 'passagemFila', 'tempo': tempoSaida,
+                           'fila': evento['fila'], 'destino': evento['destino']})
+    elif evento['evento'] == 'saida':
+        # Gera um tempo de saída aleatório e adiciona ao escalonador
+        tempoSaida = sorteio(
+            filas[evento['fila']].tempoAtendimento.split('/')[0],
+            filas[evento['fila']].tempoAtendimento.split('/')[1]) + simulacao['tempo']
+        escalonador.append(
+            {'evento': 'saida', 'tempo': tempoSaida, 'fila': evento['fila']})
+
+
+# Função para gerar números aleatórios usando um gerador linear congruente
+
 
 def geraNumerosAle(x):
     a = 1826663
     c = 858765198
     M = 2**32
     for i in range(100000):
+    # for i in range(100):
         x = (a*x + c) % M
-        # with open("pseudoaleatorio1.txt", "a") as file:
-        #     file.write(str(x/M) + "\n")
         numerosRandomicos.append(x/M)
 
+# Função para gerar a distribuição de probabilidade com base no estado da fila
+
+
 def geraDistribuicaoProbabilidade(estado, tempo):
-    return [(x/tempo) * 100 for x in estado]
-
-# Sorteio
+    return [(x / tempo) * 100 for x in estado]
 
 
-def sorteio(A, B):
-    return A + (B-A) * numerosRandomicos.pop(0)
-
-
-def addEscalonador(evento):
-    if evento == 'ch1':
-        tempoChegada = sorteio(
-            tempoChegadaA1, tempoChegadaB1) + simulacao['tempo']
-        escalonador.append({'evento': 'ch1', 'tempo': tempoChegada})
-    # if de passagem da fila 1 para 2, utiliza os tempos de saida da fila 1
-    elif evento == 'p12':
-        tempoSaida = sorteio(tempoSaidaA1, tempoSaidaB1) + simulacao['tempo']
-        escalonador.append({'evento': 'p12', 'tempo': tempoSaida})
-    else:
-        tempoSaida = sorteio(tempoSaidaA2, tempoSaidaB2) + simulacao['tempo']
-        escalonador.append({'evento': 'sa2', 'tempo': tempoSaida})
-
-
-def chegadaFila(evento, tempo):
-    global tempoTotal
-    global fila1
-    global fila2
-    global perdaFila1
-    global perdaFila2
-    global escalonador
-    simulacao['evento'] = evento
-    diferencaTempo = tempo - simulacao['tempo']
-    simulacao['tempo'] = tempo
-    # soma diferenca de tempo no estado atual da fila
-    # simulacao['estado'][simulacao['fila']] += diferencaTempo
-    
-    simulacao['estadoFila1'][simulacao['fila1']] += diferencaTempo
-    simulacao['estadoFila2'][simulacao['fila2']] += diferencaTempo
-    
-    # Checa se tem espaço fila 1
-    if (simulacao['fila1'] < capacidadeFila1):
-        simulacao['fila1'] += 1
-        # checa se tem servidor disponivel na fila 1, se tiver agenda uma passagem pra fila 2 'P12'
-        if (simulacao['fila1'] <= numeroServidores1):
-            # addEscalonador('saida')
-            addEscalonador('p12')
-    else:
-        perdaFila1 += 1
-    if len(numerosRandomicos) == 0:
-        return
-    addEscalonador('ch1')
-
-
-def saidaFila(evento, tempo):
-    global tempoTotal
-    global fila
-    global perda
-    global escalonador
-    simulacao['evento'] = evento
-    diferencaTempo = tempo - simulacao['tempo']
-    simulacao['tempo'] = tempo
-    # soma diferenca de tempo no estado atual da fila
-    # simulacao['estado'][simulacao['fila']] += diferencaTempo
-    simulacao['estadoFila1'][simulacao['fila1']] += diferencaTempo
-    simulacao['estadoFila2'][simulacao['fila2']] += diferencaTempo
-    
-    simulacao['fila2'] -= 1
-    if (simulacao['fila2'] >= numeroServidores2):
-        addEscalonador('sa2')
-        
-        
-def p12(evento, tempo):
-    global tempoTotal
-    global fila1
-    global fila2
-    global perdaFila1
-    global perdaFila2
-    global escalonador
-    simulacao['evento'] = evento
-    diferencaTempo = tempo - simulacao['tempo']
-    simulacao['tempo'] = tempo
-    # soma diferenca de tempo no estado atual da fila
-    # simulacao['estado'][simulacao['fila']] += diferencaTempo
-    simulacao['estadoFila1'][simulacao['fila1']] += diferencaTempo
-    simulacao['estadoFila2'][simulacao['fila2']] += diferencaTempo
-    
-    simulacao['fila1'] -= 1
-    if (simulacao['fila1'] >= numeroServidores1):
-        addEscalonador('p12')
-    if(simulacao['fila2'] < capacidadeFila2):
-        simulacao['fila2'] += 1
-        if(simulacao['fila2'] <= numeroServidores2):
-            addEscalonador('sa2')
-    else:
-        perdaFila2 +=1
-
-
+# Função principal para executar a simulação
 def run(x):
-    global perdaFila1
-    global perdaFila2
-    perdaFila1 = 0
-    perdaFila2 = 0
-    # Simulação
+    # Inicializa as variáveis da simulação
     global simulacao
+    global filas
+    global escalonador
+
     simulacao = {
         'evento': '-',
-        'fila1': 0,
-        'fila2': 0,
         'tempo': 0,
-        'estadoFila1': [0] * (capacidadeFila1 + 1),
-        'estadoFila2': [0] * (capacidadeFila2 + 1),
     }
-    global escalonador
-    # Escalonador
-    escalonador = [
-        {'evento': 'ch1', 'tempo': tempoInicial}
-    ]
-    # arquivo = open(arq, 'r')
-    # for linha in arquivo:
-    #     numerosRandomicos.append(float(linha))
-    # arquivo.close()
+
+    escalonador = [{'evento': 'chegada',
+                    'tempo': float(parametros['Tempo Inicial']), 'fila': 0}]
     geraNumerosAle(x)
 
-    while (len(escalonador) > 0 and len(numerosRandomicos) > 0):
+    # Loop principal da simulação
+    while escalonador and numerosRandomicos:
         escalonador.sort(key=lambda x: x['tempo'])
-        # print(escalonador)
         evento = escalonador.pop(0)
-        if (evento['evento'] == 'ch1'):
-            chegadaFila(evento['evento'], evento['tempo'])
-        elif evento['evento'] == 'sa2':
-            saidaFila(evento['evento'], evento['tempo'])
-        else:
-            p12(evento['evento'], evento['tempo'])
+        # Executa o evento com base no tipo
+        # print(escalonador)
+        if evento['evento'] == 'chegada':
+            filas[evento['fila']].chegadaFila(evento['tempo'])
+        elif evento['evento'] == 'passagemFila':
+            filas[evento['fila']].passagemFila(
+                evento['tempo'], evento['destino'])
+        elif evento['evento'] == 'saida':
+            filas[evento['fila']].saidaFila(evento['tempo'])
 
-    print("Tempo de simulação: ", evento['tempo'])
-    print("Perda de clientes fila1: ", perdaFila1)
-    print("Perda de clientes fila2: ", perdaFila2)
-    print("Distribuição de probabilidade dos estados da fila: ",
-          geraDistribuicaoProbabilidade(simulacao['estadoFila1'], simulacao['tempo']))
-    print("Distribuição de probabilidade dos estados da fila 2: ",
-          geraDistribuicaoProbabilidade(simulacao['estadoFila2'], simulacao['tempo']))
-    return simulacao, perdaFila1, perdaFila2
 
-tempoTotal = 0
-estadoFinal1 = [0] * (capacidadeFila1 + 1)
-estadoFinal2 = [0] * (capacidadeFila2 + 1)
-perdaFinalFila1 = 0
-perdaFinalFila2 = 0
+# Carrega os parâmetros do arquivo JSON
+parametros = carregaParametros()
+filas_parametros = parametros["Filas"]
 
-for i in range(5):
-    print("Simulação ", i+1)
-    sim, perd1, perd2 = run(i+1)
-    tempoTotal += sim['tempo']
-    estadoFinal1 = [x + y for x, y in zip(estadoFinal1, sim['estadoFila1'])]
-    estadoFinal2 = [x + y for x, y in zip(estadoFinal2, sim['estadoFila2'])]
-    perdaFinalFila1 += perd1
-    perdaFinalFila2 += perd2
+# Inicializa as filas com base nos parâmetros
+filas = [Fila(fila["Id"],
+              fila["Nome"],
+              fila["Tempo de Atendimento"],
+              fila["Tempo de Chegada"],
+              fila["Numero de Servidores"],
+              fila["Capacidade da Fila"],
+              fila["Probabilidade Roteamento"])
+         for fila in filas_parametros]
 
-print("Tempo médio de simulação: ", tempoTotal/5)
-print("Perda média de clientes na fila 1: ", perdaFinalFila1/5)
-print("Perda média de clientes na fila 2: ", perdaFinalFila2/5)
-print("Distribuição de probabilidade dos estados da fila 1: ",
-      geraDistribuicaoProbabilidade(estadoFinal1, tempoTotal))
-print("Distribuição de probabilidade dos estados da fila 2: ",
-      geraDistribuicaoProbabilidade(estadoFinal2, tempoTotal))
-    
+run(parametros['Semente'])
+
+print("Tempo de simulação: ", simulacao['tempo'])
+# Exibe os resultados da simulação
+for i, fila in enumerate(filas):
+    print(f"Perda de clientes da fila {i+1}: ", fila.perda)
+    print(f"Distribuição de probabilidade dos estados da fila {i+1}: ",
+          geraDistribuicaoProbabilidade(fila.estadoFila, simulacao['tempo']))
+    print(f"estadoFila {i+1}: ", fila.estadoFila)
