78f24e5ca8e74856765066159f7d3af3442bcbe5
gutobottega <guto9bottega@gmail.com>
2023-11-30 01:22:14 -0300

    maybe its right

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ numerosRandomicos = []
 
 # Definição da classe Fila, que representa uma fila no sistema
 
-
 class Fila:
     def __init__(self, id, nome, tempoAtendimento, tempoChegada, numeroServidores, capacidadeFila, probabilidadeRoteamento):
         # Atributos da fila
@@ -35,32 +34,33 @@ class Fila:
 
         for fila in filas:
             fila.estadoFila[fila.tamanhoAtual] += diferencaTempo
+            
+        # Se ainda houver capacidade na fila, adiciona um novo elemento à fila
+        if (self.capacidadeFila > self.tamanhoAtual or self.capacidadeFila == -1):
+            self.tamanhoAtual += 1
+            # Se ainda houver servidores disponíveis, adiciona o evento de passagemFila ao escalonador
+            if (self.tamanhoAtual <= self.numeroServidores):
+                 # Escolhe aleatoriamente a rota do elemento na fila
+                rota = random.choices(list(self.probabilidadeRoteamento.keys()),
+                                    weights=list(self.probabilidadeRoteamento.values()))[0]
 
-        # Escolhe aleatoriamente a rota do elemento na fila
-        rota = random.choices(list(self.probabilidadeRoteamento.keys()),
-                              weights=list(self.probabilidadeRoteamento.values()))[0]
-
-        # Se a rota for "saida", adiciona o evento de saída ao escalonador
-        if rota == "saida":
-            addEscalonador({
-                'evento': 'saida',
-                'fila': self.id,
-            })
-        else:
-            # Caso contrário, encaminha o elemento para a fila de destino
-            fila_destino = next(fila for fila in filas if fila.nome == rota)
-            # Se ainda houver capacidade na fila, adiciona um novo elemento à fila
-            if (self.capacidadeFila > self.tamanhoAtual or self.capacidadeFila == -1):
-                self.tamanhoAtual += 1
-                # Se ainda houver servidores disponíveis, adiciona o evento de passagemFila ao escalonador
-                if (self.tamanhoAtual <= self.numeroServidores):
+                # Se a rota for "saida", adiciona o evento de saída ao escalonador
+                if rota == "saida":
                     addEscalonador({
-                        'evento': 'passagemFila',
+                        'evento': 'saida',
                         'fila': self.id,
-                        'destino': fila_destino.id,
                     })
-            else:
-                self.perda += 1
+                else:
+                    # Caso contrário, encaminha o elemento para a fila de destino
+                    fila_destino = next(fila for fila in filas if fila.nome == rota)
+                addEscalonador({
+                    'evento': 'passagemFila',
+                    'fila': self.id,
+                    'destino': fila_destino.id,
+                })
+        else:
+            self.perda += 1
+            
         if len(numerosRandomicos) == 0:
             return
         addEscalonador({
@@ -68,7 +68,7 @@ class Fila:
             'fila': self.id,
         })
 
-        # Método para simular a saída de um elemento da fila
+    # Método para simular a saída de um elemento da fila
     def saidaFila(self, tempo):
         global simulacao
         global filas
@@ -80,9 +80,28 @@ class Fila:
             fila.estadoFila[fila.tamanhoAtual] += diferencaTempo
 
         self.tamanhoAtual -= 1
+        # Se ainda houver elementos para serem atendidos na fila, adiciona o evento um evento ao escalonador
+        if (self.tamanhoAtual >= self.numeroServidores):
+            rota = random.choices(list(self.probabilidadeRoteamento.keys()),
+                                  weights=list(self.probabilidadeRoteamento.values()))[0]
+            # Se a rota for "saida", adiciona o evento de saída ao escalonador
+            if rota == "saida":
+                addEscalonador({
+                    'evento': 'saida',
+                    'fila': self.id,
+                })
+            else:
+                # Caso contrário, encaminha o elemento para a fila de destino
+                fila_destino = next(
+                    fila for fila in filas if fila.nome == rota)
+                addEscalonador({
+                    'evento': 'passagemFila',
+                    'fila': self.id,
+                    'destino': fila_destino.id,
+                })
+
 
     # Método para simular o passasgem de um elemento na fila
-
     def passagemFila(self, tempo, destino):
         global simulacao
         global filas
@@ -112,6 +131,7 @@ class Fila:
                     'fila': self.id,
                     'destino': fila_destino.id,
                 })
+
         # Se ainda houver capacidade na fila, adiciona um novo elemento à fila
         if (filas[destino].tamanhoAtual < filas[destino].capacidadeFila or self.capacidadeFila == -1):
             filas[destino].tamanhoAtual += 1
@@ -137,9 +157,9 @@ class Fila:
                     })
         else:
             filas[destino].perda += 1
+
+
 # Função para carregar parâmetros de configuração do arquivo JSON
-
-
 def carregaParametros():
     with open('config.json', 'r') as file:
         parametros = json.load(file)
@@ -187,7 +207,6 @@ def geraNumerosAle(x):
     c = 858765198
     M = 2**32
     for i in range(100000):
-    # for i in range(100):
         x = (a*x + c) % M
         numerosRandomicos.append(x/M)
 
@@ -251,4 +270,3 @@ for i, fila in enumerate(filas):
     print(f"Perda de clientes da fila {i+1}: ", fila.perda)
     print(f"Distribuição de probabilidade dos estados da fila {i+1}: ",
           geraDistribuicaoProbabilidade(fila.estadoFila, simulacao['tempo']))
-    print(f"estadoFila {i+1}: ", fila.estadoFila)
